6d5925e68f42f9761f05abf28cdd7b8ce4c9760d
Khunt Arpit <arpitkhunt66@gmail.com>
2022-09-18 10:53:01 +0530

    first commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <Navbar variant="light" bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <Link
+            style={{ textDecoration: "none", color: "#333333" }}
+            to="/"
+          >
+            React-Bootstrap
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-dark-example" />
         <Navbar.Collapse id="navbar-dark-example">
           <Nav>
