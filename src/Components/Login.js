daf6b78b223be36733bfbbaf350992b8b7fece19
Khunt Arpit <arpitkhunt66@gmail.com>
2022-09-21 23:00:54 +0530

    FIrebase Google Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Button, Container, Form } from "react-bootstrap";
-import { auth } from "../firebase-config";
+import { auth, signInWithGoogle } from "../firebase-config";
+import { addItem } from "../utils";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -12,18 +13,22 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await signInWithEmailAndPassword(auth, email, password);
-    navigate('/');
+    const result = await signInWithEmailAndPassword(auth, email, password);
+    console.log(`result`, result)
+    // addItem("profile", { name: result.fullName, email: result._tokenResponse.email, photoUrl: result._tokenResponse.photoUrl });
+    navigate("/");
+  };
+  
+  const googleSignIn = async () => {
+    const result = await signInWithGoogle();
+    addItem("profile", { name: result._tokenResponse.fullName, email: result._tokenResponse.email, photoUrl: result._tokenResponse.photoUrl });
+    navigate("/");
   };
 
   return (
-    <div>
-      <Container className="m-5 d-flex justify-content-center">
-        <Form
-          className="p-5 rounded"
-          style={{ width: "50%", border: "2px solid #666666" }}
-          onSubmit={(e) => handleSubmit(e)}
-        >
+    <div className="d-flex justify-content-center">
+      <Container className="formWrapper">
+        <Form onSubmit={(e) => handleSubmit(e)}>
           <Form.Group className="mb-2">
             <Form.Label htmlFor="email">Email</Form.Label>
             <Form.Control
@@ -41,14 +46,24 @@ function Login() {
             />
           </Form.Group>
           <Form.Group className="mt-3">
-            <Button className="me-1" type="submit">
-              Submit
-            </Button>
-            <Button className="ms-1" variant="danger">
-              Cancel
+            <Button className="submitButton" type="submit">
+              Continue
             </Button>
           </Form.Group>
         </Form>
+        <span className="mt-3 text-center">OR</span>
+        <div className="googleLogin">
+          <img
+            className="googleLogo"
+            src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4="
+          />
+          <Button
+            className="googleButton"
+            onClick={googleSignIn}
+          >
+            Continue with Google
+          </Button>
+        </div>
       </Container>
     </div>
   );
