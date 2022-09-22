23c508cd556ebbd2293471a76ad437f0080dc60f
Khunt Arpit <arpitkhunt66@gmail.com>
2022-09-22 08:30:19 +0530

    google auth integration

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Button, Container, Form } from "react-bootstrap";
 import { auth, signInWithGoogle } from "../firebase-config";
-import { addItem } from "../utils";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -13,15 +12,12 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const result = await signInWithEmailAndPassword(auth, email, password);
-    console.log(`result`, result)
-    // addItem("profile", { name: result.fullName, email: result._tokenResponse.email, photoUrl: result._tokenResponse.photoUrl });
+    await signInWithEmailAndPassword(auth, email, password);
     navigate("/");
   };
-  
+
   const googleSignIn = async () => {
-    const result = await signInWithGoogle();
-    addItem("profile", { name: result._tokenResponse.fullName, email: result._tokenResponse.email, photoUrl: result._tokenResponse.photoUrl });
+    await signInWithGoogle();
     navigate("/");
   };
 
@@ -57,10 +53,7 @@ function Login() {
             className="googleLogo"
             src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4="
           />
-          <Button
-            className="googleButton"
-            onClick={googleSignIn}
-          >
+          <Button className="googleButton" onClick={googleSignIn}>
             Continue with Google
           </Button>
         </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../firebase-config";
-import '../App.css'
+import { addItem } from '../utils';
+import "../App.css";
 
 function NavBar() {
   const [userProfile, setUserProfile] = useState(null);
@@ -11,30 +12,53 @@ function NavBar() {
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
+      addItem("profile", { token: currentUser.uid })
       setUserProfile(currentUser);
     });
-  }, [userProfile])
+  }, [userProfile]);
 
   const logout = async () => {
-    navigate('/login');
+    navigate("/login");
     await signOut(auth);
-  }
+  };
 
   return (
     <header className="header">
       <nav className="navbar">
         <ul className="left">
-          <li><Link to="/"><img src="/logo.png" /></Link></li>
-          <li><Link className="links" to="/users">Users</Link></li>
-          <li><Link className="links" to="/posts">Posts</Link></li>
+          <li>
+            <Link to="/">
+              <img src="/logo.png" />
+            </Link>
+          </li>
+          <li>
+            <Link className="links" to="/users">
+              Users
+            </Link>
+          </li>
+          <li>
+            <Link className="links" to="/posts">
+              Posts
+            </Link>
+          </li>
         </ul>
         <ul className="right">
           <li>
-            {userProfile ?
-              <Link className="links" onClick={logout}>{userProfile?.email?.split("@")[0]}</Link>
-              :
-              <Link className="links" to="/login">Login</Link>
-            }
+            {userProfile ? (
+              userProfile?.photoURL ? (
+                <img className="rounded-circle" style={{ width: "40px", height: "40px" }} src={userProfile.photoURL} onClick={() => logout()} />
+              ) : (
+                <Link className="links" onClick={() => logout()}>
+                  {userProfile.displayName
+                    ? userProfile.displayName
+                    : userProfile?.email?.split("@")[0]}
+                </Link>
+              )
+            ) : (
+              <Link className="links" to="/login">
+                Login
+              </Link>
+            )}
           </li>
         </ul>
       </nav>
